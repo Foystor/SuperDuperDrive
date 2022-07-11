7cc36d792972702575c116f191c5ba5330c35550
Foystor <foystor@gmail.com>
2022-07-11 17:29:20 +0800

    feat: Implement simple file uploading

--- cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -1,8 +1,12 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import com.udacity.jwdnd.course1.cloudstorage.model.File;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/home")
@@ -11,5 +15,17 @@ public class HomeController {
     @GetMapping
     public String homeView() {
         return "home";
+    }
+
+    @PostMapping
+    public String uploadFile(@RequestParam("fileUpload") MultipartFile file, Model model) throws IOException {
+
+        // check if file is empty
+        if (file.isEmpty()) {
+            model.addAttribute("uploadErrorMsg","Please select a file to upload.");
+        } else {
+            model.addAttribute("success",true);
+        }
+        return "result";
     }
 }
