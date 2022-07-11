3d9b704982c4fe9dc15483d1e1f92376823c0a54
Foystor <foystor@gmail.com>
2022-07-11 19:37:20 +0800

    feat: Implement dynamic file uploading

--- cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -1,6 +1,10 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 import com.udacity.jwdnd.course1.cloudstorage.model.File;
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -12,8 +16,15 @@ import java.io.IOException;
 @RequestMapping("/home")
 public class HomeController {
 
+    private final FileService fileService;
+
+    public HomeController(FileService fileService) {
+        this.fileService = fileService;
+    }
+
     @GetMapping
-    public String homeView() {
+    public String homeView(Model model) {
+        model.addAttribute("fileList", fileService.getFiles());
         return "home";
     }
 
@@ -24,6 +35,7 @@ public class HomeController {
         if (file.isEmpty()) {
             model.addAttribute("uploadErrorMsg","Please select a file to upload.");
         } else {
+            fileService.CreateFile(file);
             model.addAttribute("success",true);
         }
         return "result";
