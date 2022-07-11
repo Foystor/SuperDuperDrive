a93465d5cc1d9a14270eff47a6f30e06af3df452
Foystor <foystor@gmail.com>
2022-07-11 14:35:46 +0800

    feat: Implement logout

---
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LogoutController.java
@@ -0,0 +1,22 @@
+package com.udacity.jwdnd.course1.cloudstorage.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class LogoutController {
+
+    @RequestMapping("/logout")
+    public String logout(Authentication authentication, HttpServletRequest request, HttpServletResponse response) {
+        if (authentication != null)
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+
+        return "redirect:/login";
+    }
+}
